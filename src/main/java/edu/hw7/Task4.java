530ea0048fa1fe8dd68d074ae42a686e058b6b3b
svp8 <azaz08082001@gmail.com>
2023-11-28 21:45:29 +0300

    fixed

--- src/main/java/edu/hw7/Task4.java
+++ src/main/java/edu/hw7/Task4.java
@@ -3,7 +3,8 @@ package edu.hw7;
 import java.security.SecureRandom;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.atomic.LongAdder;
 import static java.lang.Math.pow;
 
 public final class Task4 {
@@ -31,28 +32,31 @@ public final class Task4 {
     }
 
     public static double calculatePiParallel(int n, int threadCount) throws InterruptedException {
-        final AtomicInteger totalCount = new AtomicInteger(0);
-        final AtomicInteger circleCount = new AtomicInteger(0);
-        SecureRandom random = new SecureRandom();
+        final LongAdder totalCount = new LongAdder();
+        final LongAdder circleCount = new LongAdder();
+        int left = n % threadCount;
         Runnable task = () -> {
             for (int i = 0; i < n / threadCount; i++) {
-                if ((pow(random.nextDouble(0, 1), 2) + pow(random.nextDouble(0, 1), 2)) < 1) {
-                    circleCount.incrementAndGet();
+                if ((pow(ThreadLocalRandom.current().nextDouble(0, 1), 2)
+                    + pow(ThreadLocalRandom.current().nextDouble(0, 1), 2)) < 1) {
+                    circleCount.increment();
                 }
-                totalCount.incrementAndGet();
+                totalCount.increment();
+            }
+        };
+        Runnable leftTask = () -> {
+            for (int i = 0; i < left; i++) {
+                if ((pow(ThreadLocalRandom.current().nextDouble(0, 1), 2)
+                    + pow(ThreadLocalRandom.current().nextDouble(0, 1), 2)) < 1) {
+                    circleCount.increment();
+                }
+                totalCount.increment();
             }
         };
         try (ExecutorService executor = Executors.newFixedThreadPool(threadCount)) {
+            executor.execute(leftTask);
             for (int i = 0; i < threadCount; i++) {
                 executor.execute(task);
-            }
-        }
-        if (totalCount.get() < n) {
-            for (int i = 0; i < n - totalCount.get(); i++) {
-                if ((pow(random.nextDouble(0, 1), 2) + pow(random.nextDouble(0, 1), 2)) < 1) {
-                    circleCount.incrementAndGet();
-                }
-                totalCount.incrementAndGet();
             }
         }
         double pi = FOUR * (circleCount.doubleValue() / totalCount.doubleValue());
--- src/test/java/edu/hw7/Task4Test.java
+++ src/test/java/edu/hw7/Task4Test.java
@@ -3,44 +3,49 @@ package edu.hw7;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class Task4Test {
     Logger logger = LogManager.getLogger();
 
-    @Test
-    void calculatePi() {
-        long start = System.currentTimeMillis();
-        double pi = Task4.calculatePi(1000);
+    @ParameterizedTest()
+    @ValueSource(ints = {10000, 100000, 10_000_000, 100_000_000})
+    void calculatePi(int n) {
+        long start = System.nanoTime();
+        double pi = Task4.calculatePi(n);
         logger.info(pi);
-        long end = System.currentTimeMillis();
-        logger.info(3.14159265359 - pi);
-        logger.info(end - start);
+        long end = System.nanoTime();
+        logger.info("Ошибка: " + (3.14159265359 - pi));
+        logger.info("Время: " + (end - start));
     }
 
     //10тыс 0.0040073, 100тыс 0.0089526, 1млн 0.00086 и 10млн -1.8694640999994405E-4 91732
     //test 10mln 1min17sec
-    @Test
-    void calculatePiParallel() throws InterruptedException {
-        long start = System.currentTimeMillis();
-        logger.info(Task4.calculatePiParallel(100000,4));
-        long end = System.currentTimeMillis();
-        logger.info(end - start);
+    @ParameterizedTest
+    @CsvSource({"100003,2", "100003,3", "100003,4", "100003,5", "1000030,2", "1000030,3", "1000030,4"})
+    void calculatePiParallel(int n, int threads) throws InterruptedException {
+        long start = System.nanoTime();
+        logger.info(Task4.calculatePiParallel(n, threads));
+        long end = System.nanoTime();
+        logger.info((end - start) / 1000000);
     }
 
-    @Test
-    void compare() throws InterruptedException {
-        int n = 1;
-        long start = System.currentTimeMillis();
+    @ParameterizedTest
+    @CsvSource({"2,4", "100003,2", "100003,3", "100003,4", "100003,5", "1000030,2", "1000030,3", "1000030,4"})
+    void compare(int n, int threads) throws InterruptedException {
+        long start = System.nanoTime();
         logger.info(Task4.calculatePi(n));
-        long end = System.currentTimeMillis();
-        logger.info((end - start));
+        long end = System.nanoTime();
+        logger.info("Время последовательно: " + (end - start) / 1000000);
 
-        start = System.currentTimeMillis();
-        logger.info(Task4.calculatePiParallel(n,4));
-        end = System.currentTimeMillis();
-        logger.info((end - start));
+        start = System.nanoTime();
+        logger.info(Task4.calculatePiParallel(n, threads));
+        end = System.nanoTime();
+        logger.info("Время параллельно : " + (end - start) / 1000000);
     }
     //1mln
     //9916 9451 5 potokov
